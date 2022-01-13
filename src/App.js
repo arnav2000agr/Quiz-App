39b38b5d2d7ba8c5dba9c2ab0c460fd800d0a96a
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2022-01-13 21:15:58 +0530

    Added Questions for the quiz

--- src/App.js
+++ src/App.js
@@ -4,41 +4,95 @@ function App() {
 
   const questions = [
     {
-      questionText: ' What is the extension of java code  ?',
+      questionText: 'Which of the following is not a Java features?',
       answerOptions: [
         { answerText: 'Dynamic', isCorrect: false },
-        { answerText: 'Architecture', isCorrect: false },
+        { answerText: 'Portable', isCorrect: false },
         { answerText: 'Use of Pointers', isCorrect: true },
         { answerText: 'Object Oriented', isCorrect: false },
       ],
     },
     {
-      questionText: 'Who is CEO of Tata?',
+      questionText: 'Which component is used to compile, debug and execute the java programs?',
       answerOptions: [
-        { answerText: 'Jeff Bezos', isCorrect: false },
-        { answerText: 'Ratan Tata', isCorrect: true },
-        { answerText: 'Mukesh Ambani', isCorrect: false },
-        { answerText: 'Gautam Adani', isCorrect: false },
+        { answerText: 'JRE', isCorrect: false },
+        { answerText: 'JDK', isCorrect: true },
+        { answerText: 'JIT', isCorrect: false },
+        { answerText: 'JVM', isCorrect: false },
       ],
     },
     {
-      questionText: 'who is richest person in the world?',
+      questionText: 'What is the extension of java code files?',
       answerOptions: [
-        { answerText: 'Jeff Bezos', isCorrect: false },
-        { answerText: 'Elon Musk', isCorrect: true },
-        { answerText: 'Mukesh Ambani', isCorrect: false },
-        { answerText: 'Warren Buffett', isCorrect: false },
+        { answerText: '.js', isCorrect: false },
+        { answerText: '.java', isCorrect: true },
+        { answerText: '.txt', isCorrect: false },
+        { answerText: '.class', isCorrect: false },
       ],
     },
     {
-      questionText: 'how many countries in the world?',
+      questionText: 'What is Truncation in Java?',
       answerOptions: [
-        { answerText: '120', isCorrect: false },
-        { answerText: '183', isCorrect: false },
-        { answerText: '170', isCorrect: false },
-        { answerText: '195', isCorrect: true },
+        { answerText: 'Floating-point value assigned to a Floating type', isCorrect: false },
+        { answerText: 'Integer value assigned to Integer type', isCorrect: false },
+        { answerText: 'Integer value assigned to floating type', isCorrect: false },
+        { answerText: 'Floating-point value assigned to an integer type', isCorrect: true },
       ],
     },
+    {
+      questionText: ' What is the extension of compiled java classes?',
+      answerOptions: [
+        { answerText: '.js', isCorrect: false },
+        { answerText: '.java', isCorrect: false },
+        { answerText: '.txt', isCorrect: false },
+        { answerText: '.class', isCorrect: true },
+      ],
+    },
+    {
+      questionText: 'Which of these keywords is used to define interfaces in Java?',
+      answerOptions: [
+        { answerText: 'interface', isCorrect: true },
+        { answerText: 'Interface', isCorrect: false },
+        { answerText: 'intf', isCorrect: false },
+        { answerText: 'Intf', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Which of the following is a superclass of every class in Java?',
+      answerOptions: [
+        { answerText: ' Abstract class', isCorrect: false },
+        { answerText: 'ArrayList', isCorrect: false },
+        { answerText: 'Object class', isCorrect: true },
+        { answerText: 'String', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Which of these packages contains the exception Stack Overflow in Java?',
+      answerOptions: [
+        { answerText: 'java.io', isCorrect: false },
+        { answerText: 'java.lang', isCorrect: true },
+        { answerText: 'java.system', isCorrect: false },
+        { answerText: 'java.util', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Which of these keywords are used for the block to be examined for exceptions?',
+      answerOptions: [
+        { answerText: 'check', isCorrect: false },
+        { answerText: 'throw', isCorrect: false },
+        { answerText: 'catch', isCorrect: false },
+        { answerText: 'try', isCorrect: true },
+      ],
+    },
+    {
+      questionText: 'Which one of the following is not an access modifier?',
+      answerOptions: [
+        { answerText: 'void', isCorrect: true},
+        { answerText: 'public', isCorrect: false },
+        { answerText: 'protected', isCorrect: false },
+        { answerText: 'private', isCorrect: false },
+      ],
+    }
   ]
 
   const [currentQuestion, setCurrentQuestion] = useState(0)
@@ -66,7 +120,7 @@ function App() {
       <div id="headings2">
       <h1 className='mainheading2'>QuizMate</h1>
       </div>
-      <h1 className='header'>Quiz</h1>
+      <h1 className='header'>Java Quiz</h1>
       <div className="app">
       
         {showScore ? (
@@ -82,7 +136,7 @@ function App() {
             <>
               <div className='question-section'>
                 <div className='question-count'>
-                  <span>Question {currentQuestion + 1}</span>{questions.length}
+                  <span>Question {currentQuestion + 1}</span>/{questions.length}
                 </div>
                 <div className='question-text'>
                   {questions[currentQuestion].questionText}
